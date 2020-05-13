2ad4ec99e797b1096046931ba577c469f5ff3636
Pawxon <zelazek_pawel@wp.pl>
2020-05-13 16:36:31 +0200

    lab7

---
+++ lab7/Assets/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 3;
+    int y = 0;
+
+        while (y < x)
+        {
+            print("Paweł");
+            y++;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
