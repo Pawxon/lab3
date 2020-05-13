d6fa7e1c3b21e8e4dfdbc5bc7c936160ce009e1a
Pawxon <zelazek_pawel@wp.pl>
2020-05-14 01:02:26 +0200

    lab8

---
+++ lab8/Assets/s1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+
+    GameObject[] szukane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        szukane = GameObject.FindGameObjectsWithTag("kula");
+        foreach (GameObject s in szukane)
+        {
+            Destroy(s);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
