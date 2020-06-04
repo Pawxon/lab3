e8c2215471eafee0ee3f4f54b701a6963b3f3092
Pawxon <zelazek_pawel@wp.pl>
2020-06-04 18:16:31 +0200

    lab10

---
+++ lab10/Assets/kulka.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kulka : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    public GameObject [] kuleczka;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, y, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pozycja = Input.mousePosition;
+            pozycja.z = 15f;
+            pozycja = Camera.main.ScreenToWorldPoint(pozycja);
+            int los = Random.Range(0, kuleczka.Length);
+            Instantiate(kuleczka[los], pozycja, Quaternion.identity);
+        }
+        
+
+    }
+}
